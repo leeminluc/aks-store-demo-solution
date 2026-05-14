a403c01712a98c8e90e016fe5c9d46d219188f8c
leeminluc <jonluc_julian@yahoo.com>
2026-05-14 20:03:25 +0800

    modify output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,23 +5,23 @@
 # Resource Group Name
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = module.aks_store.resource_group_name
+  value       = module.aks_store.AZURE_RESOURCE_GROUP
 }
 
 # AKS Cluster Name
 output "aks_cluster_name" {
   description = "The name of the AKS cluster"
-  value       = module.aks_store.aks_cluster_name
+  value       = module.aks_store.AZURE_AKS_CLUSTER_NAME
 }
 
 # ACR Login Server
 output "acr_login_server" {
   description = "The login server URL of the Azure Container Registry"
-  value       = module.aks_store.acr_login_server
+  value       = module.aks_store.AZURE_CONTAINER_REGISTRY_ENDPOINT
 }
 
 # ACR Name (without .azurecr.io suffix)
 output "acr_name" {
   description = "The name of the Azure Container Registry (without .azurecr.io suffix)"
-  value       = module.aks_store.acr_name
+  value       = module.aks_store.AZURE_CONTAINER_REGISTRY_NAME
 }
